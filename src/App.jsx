3512a8fca32a52be7540ceef2f77e6d19b944901
MIRALISTA <mironovatf7@gmail.com>
2020-03-11 12:14:06 +1300

    tanya

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 
-
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        
-        
+        <div>Hello</div>
       </React.Fragment>
     );
   }
