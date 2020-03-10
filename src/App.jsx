b2054763b16d5bbee6b5c0ce0805d9385bae5527
nakitnaidu <nainai.nikita@gmail.com>
2020-03-11 12:17:37 +1300

    nikita

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>Hello</div>
+        <div>Hello from nikits</div>
       </React.Fragment>
     );
   }
