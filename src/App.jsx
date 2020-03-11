07b1091923bb2cb663c2d207cc74f40aeb8ee5a4
MIRALISTA <mironovatf7@gmail.com>
2020-03-12 09:14:25 +1300

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-
+import { Router } from "@reach/router";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import ViewCandidates from "./components/ViewCandidates";
+import AddCandidate from "./components/AddCandidate";
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navigation />
+        <Router>
+          <Home path="/" />
+          <ViewCandidates path="/viewcandidates" />
+          <AddCandidate path="/addcandidate" />
+        </Router>
         <div>Hello from nikits</div>
       </React.Fragment>
     );
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+const menuStyle = {
+  display: "flex",
+  width: "80vw",
+  justifyContent: "space-between",
+  alignItems: "center",
+  listStyleType: "none"
+};
+
+const LinkStyle = { color: "#4caf50" };
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <ul style={menuStyle}>
+          <li>
+            <Link style={LinkStyle} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link style={LinkStyle} to="/viewcandidates">
+              View Candidates
+            </Link>
+          </li>
+          <li>
+            <Link style={LinkStyle} to="/addcandidate">
+              Add Candidate
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
